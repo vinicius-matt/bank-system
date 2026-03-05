a9d33fe209bd28aebe20abee3b3d7a69dfd29e3d
Matheus V <matheusvini754@gmail.com>
2026-03-05 17:42:01 -0300

    first commit

---
+++ src/main/java/com/Bank/bank_system/BankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Bank.bank_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemApplication.class, args);
+	}
+
+}
