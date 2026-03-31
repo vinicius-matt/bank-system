b6d66e8c0d18de89b77cf888f045d2d2d54cc15e
Matheus <matheusvini754@gmail.com>
2026-03-30 21:29:14 -0300

    fix class cliente controller, utilizando a service ao invés da repository

--- src/main/java/com/Bank/bank_system/Controller/ClienteController.java
+++ src/main/java/com/Bank/bank_system/Controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.Bank.bank_system.Controller;
 
 import com.Bank.bank_system.Entity.Cliente;
 import com.Bank.bank_system.Repository.ClienteRepository;
+import com.Bank.bank_system.Service.ClienteService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cliente")
 public class ClienteController {
 
-    private final ClienteRepository clienteRepository;
+    private final ClienteService clienteService;
 
-    public ClienteController (ClienteRepository clienteRepository){
-        this.clienteRepository = clienteRepository;
+    public ClienteController (ClienteService clienteService){
+        this.clienteService = clienteService;
     }
 
     @PostMapping("/criar")
     public Cliente criarCliente(@RequestBody Cliente cliente){
-        return clienteRepository.save(cliente);
+        return clienteService.criarCliente(cliente);
     }
 }
