dbc9dc5e27a903938a58fcc65e90d047f5c3d783
Matheus <matheusvini754@gmail.com>
2026-06-08 13:29:42 -0300

    refatorado consulta de saldo

--- src/main/java/com/Bank/bank_system/Service/ContaService.java
+++ src/main/java/com/Bank/bank_system/Service/ContaService.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 @Service
--- src/main/java/com/Bank/bank_system/dto/SaldoResponseDTO.java
+++ src/main/java/com/Bank/bank_system/dto/SaldoResponseDTO.java
@@ -1,5 +1,6 @@
 package com.Bank.bank_system.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class SaldoResponseDTO {
 
     private BigDecimal saldo;
