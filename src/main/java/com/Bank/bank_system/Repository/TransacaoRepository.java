7220db759a25bea8524b26256d79926142c4c84f
Matheus Miranda <matheusvini754@gmail.com>
2026-06-19 02:15:28 -0300

    metodo novo

--- src/main/java/com/Bank/bank_system/Repository/TransacaoRepository.java
+++ src/main/java/com/Bank/bank_system/Repository/TransacaoRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 
     List<Transacao> findByContaId(Long contaId);
+
+    List<Transacao> findByContaIdOrderByDataDesc(Long contaId);
 }
