e61911f915e392f63393285a396dab89c294331e
Matheus Miranda <matheusvini754@gmail.com>
2026-06-19 01:11:36 -0300

    configurado cors,readme e app properties

--- src/main/java/com/Bank/bank_system/Controller/ClienteController.java
+++ src/main/java/com/Bank/bank_system/Controller/ClienteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
 
     private final ClienteService clienteService;
