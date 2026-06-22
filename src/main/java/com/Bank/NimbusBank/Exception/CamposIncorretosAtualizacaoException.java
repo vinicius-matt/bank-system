014c6dd70eb502da0500ceba56b4e302248ea06c
Matheus Miranda <matheusvini754@gmail.com>
2026-06-21 23:41:57 -0300

    alterado nome do projeto

--- src/main/java/com/Bank/NimbusBank/Exception/CamposIncorretosAtualizacaoException.java
+++ src/main/java/com/Bank/NimbusBank/Exception/CamposIncorretosAtualizacaoException.java
@@ -1,4 +1,4 @@
-package com.Bank.bank_system.Exception;
+package com.Bank.NimbusBank.Exception;
 
 public class CamposIncorretosAtualizacaoException extends RuntimeException {
     public CamposIncorretosAtualizacaoException(String message) {
